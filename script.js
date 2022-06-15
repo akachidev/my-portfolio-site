4f9d6ab2143cc37bf24043bbafe2ea231a451d54
Tutorials-Git <codewithprince.com@gmail.com>
2022-06-15 03:50:41 +0100

    New project to push to github

---
+++ script.js
@@ -0,0 +1,22 @@
+window.onload = () => {
+    setTimeout(() => {
+        document.querySelector('body').classList.add
+        ('display');
+    },4000);
+};
+
+
+
+
+document.querySelector(".handburger-menu").
+addEventListener('click', () => {
+document.querySelector(".container").
+classList.toggle("change");
+});
+
+document.querySelector(".scroll-btn").addEventListener("click", () => {
+    document.querySelector("html").style.scrollBehavior = "smooth";
+    setTimeout(() => {
+        document.querySelector("html").style.scrollBehavior = "unset";
+    },1000);
+});
